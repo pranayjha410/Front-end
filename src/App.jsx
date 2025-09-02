cde95220931ddd2d24ecec46e16a840367b61017
pranayjha410 <pranayjha410@gmail.com>
2025-09-02 11:28:05 +0530

    Used React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import CartPage from "./Componet/CartPage";
 import Home from "./Componet/Home";
 import { Routes,Route } from "react-router-dom";
+import Navbar from "./Componet/Navbar";
 function App() {
   return (
     <div >
+      <Navbar/>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/product" element={<div>Products Page Coming Soon</div>} />
+      <Route path="/about" element={<div>About Us Page Coming Soon</div>} />
+      <Route path="/contact" element={<div>Contact Page Coming Soon</div>} />
+
       <Route path="/CartPage" element={<CartPage />} />
       </Routes>
     </div>
--- src/Componet/Navbar.jsx
+++ src/Componet/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Cart from "./Cart";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Navbar = ({ cartCount }) => {
+  const location = useLocation();
+
   return (
     <header className="bg-gray-900 text-white shadow-md w-full">
       <div className="max-w-7xl mx-auto flex justify-between items-center px-6 py-4">
-        
         {/* Logo */}
         <div className="flex items-center gap-2">
           <div className="bg-pink-500 p-2 rounded-full">
@@ -28,15 +29,58 @@ const Navbar = ({ cartCount }) => {
 
         {/* Nav links */}
         <nav className="hidden md:flex gap-6 text-base text-green-400">
-          <Link to='/' className="hover:text-white cursor-pointer">Home</Link>
-          <Link to='/product' className="hover:text-white cursor-pointer">Proudct</Link>
-          <Link to='/bout' className="hover:text-white cursor-pointer">About</Link>
-          <Link to='/contect' className="hover:text-white cursor-pointer">Contect Us</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `cursor-pointer hover:text-white ${
+                isActive ? "text-blue-400 font-semibold underline" : ""
+              }`
+            }
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/product"
+            className={({ isActive }) =>
+              `cursor-pointer hover:text-white ${
+                isActive ? "text-blue-400  font-semibold underline" : ""
+              }`
+            }
+          >
+            Product
+          </NavLink>
+
+          <NavLink
+            to="/about"
+            className={({ isActive }) =>
+              `cursor-pointer hover:text-white ${
+                isActive ? "text-blue-400  font-semibold underline" : ""
+              }`
+            }
+          >
+            About
+          </NavLink>
+
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              `cursor-pointer hover:text-white ${
+                isActive ? "text-blue-400  font-semibold underline" : ""
+              }`
+            }
+          >
+            Contact Us
+          </NavLink>
         </nav>
 
         {/* Cart icon */}
         <div className="flex items-center">
-         <Link to='/cartPage'><Cart cartCount={cartCount} /></Link> 
+          {location.pathname !== "/CartPage" && (
+            <NavLink to="/CartPage">
+              <Cart cartCount={cartCount} />
+            </NavLink>
+          )}
         </div>
       </div>
     </header>
